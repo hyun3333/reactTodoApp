7c6ef5a3779e0d4622e535baad5817e09c27a0d1
hyun <hyun231366@gmail.com>
2023-06-28 10:16:06 +0900

    오류 해결

--- src/main/java/com/example/todo/userapi/service/UserService.java
+++ src/main/java/com/example/todo/userapi/service/UserService.java
@@ -1,12 +1,14 @@
 package com.example.todo.userapi.service;
 
 import com.example.todo.auth.TokenProvider;
+import com.example.todo.auth.TokenUserInfo;
 import com.example.todo.exception.DuplicatedEmailException;
 import com.example.todo.exception.NoRegisteredArgumentsException;
 import com.example.todo.userapi.dto.UserSignUpResponseDTO;
 import com.example.todo.userapi.dto.request.LoginRequestDTO;
 import com.example.todo.userapi.dto.request.UserRequestSignUpDTO;
 import com.example.todo.userapi.dto.response.LoginResponseDTO;
+import com.example.todo.userapi.entity.Role;
 import com.example.todo.userapi.entity.User;
 import com.example.todo.userapi.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -23,9 +25,9 @@ public class UserService {
     private final PasswordEncoder encoder;
     private final TokenProvider tokenProvider;
 
+    //회원 가입 처리
     public UserSignUpResponseDTO create(final UserRequestSignUpDTO dto)
-        throws RuntimeException{
-
+            throws RuntimeException {
 
         String email = dto.getEmail();
         if(dto == null) {
@@ -36,11 +38,10 @@ public class UserService {
             log.warn("이메일이 중복되었습니다. - {}", email);
             throw new DuplicatedEmailException("중복된 이메일 입니다.");
         }
-        
+
         //패스워드 인코딩
         String encoded = encoder.encode(dto.getPassword());
         dto.setPassword(encoded);
-
 
         //유저 엔터티로 변환
         User user = dto.toEntity();
@@ -50,8 +51,6 @@ public class UserService {
         log.info("회원 가입 정상 수행됨! - saved user - {}", saved);
 
         return new UserSignUpResponseDTO(saved);
-
-
     }
 
     public boolean isDuplicate(String email) {
@@ -64,7 +63,7 @@ public class UserService {
         // 이메일을 통해 회원 정보를 조회.
         User user = userRepository.findByEmail(dto.getEmail())
                 .orElseThrow(
-                        () -> new RuntimeException("가입된 회원이 아닙니다.")
+                        () -> new RuntimeException("가입된 회원이 아닙니다!")
                 );
 
         //패스워드 검증
@@ -82,6 +81,44 @@ public class UserService {
         String token = tokenProvider.createToken(user);
 
         return new LoginResponseDTO(user, token);
-
     }
+
+
+    //프리미엄으로 등급 업
+    public LoginResponseDTO promoteToPremium(TokenUserInfo userInfo)
+            throws NoRegisteredArgumentsException, IllegalStateException
+    {
+
+        User foundUser = userRepository
+                .findById(userInfo.getUserId())
+                .orElseThrow(
+                        () -> new NoRegisteredArgumentsException("회원 조회에 실패!")
+                );
+
+        // 일반 회원이 아니면 예외
+        if (userInfo.getRole() != Role.COMMON) {
+            throw new IllegalStateException("일반 회원이 아니면 등급을 상승시킬 수 없습니다.");
+        }
+
+        //등급 변경
+        foundUser.changeRole(Role.PREMIUM);
+        User saved = userRepository.save(foundUser);
+
+        // 토큰을 재발급
+        String token = tokenProvider.createToken(saved);
+
+        return new LoginResponseDTO(saved, token);
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
